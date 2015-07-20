d229d9e0cb8f99709b36ecba0d5827f0d6f0d168
zing <zinglee@vip.qq.com>
2015-07-20 16:49:51 +0800

    py转exe脚本

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+# -*- coding: utf-8 -*-
+
+##############################################
+# Filename: setup.py
+# Mtime: 2015/7/20 16:33
+# Description:
+#    用于生成exe文件
+# Author: Zing
+##############################################
+
 from distutils.core import setup
 import py2exe
 
 setup(
-	windows=[
-		{"script":"main.py","icon_resources":[(1,"logo.ico"),]}],
-	options={
-		"py2exe":{"includes":["sip"],"dll_excludes":["MSVCP90.dll"]}},
-	data_files=[
-		("image", ["image/logo.png", "image/code.jpg"])]
-	)
+    windows=[
+        {"script":"main.py","icon_resources":[(1,"logo.ico"),]}],
+    options={
+        "py2exe":{"includes":["sip"],"dll_excludes":["MSVCP90.dll"]}},
+    data_files=[
+        ("image", ["image/logo.png", "image/code.jpg", "image/loading.png"\
+        	, "image/nodata.png", "image/please.png", "image/s_nodata.png"])]
+    )
